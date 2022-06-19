434bdf7aeb1ee68936bcc6ee445873e422969736
python290821 <python290821@gmail.com>
2022-06-19 20:05:50 +0300

    upload-all

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import Car from "./components/Car";
 import "./App.css";
+import Garage from "./components/Garage";
 
 class App extends Component {
+  static my_car_seq = 4
+  state = {
+    my_cars: [
+      { brand: "Honda", model: "civic", color: "blue", year: 2018, id: 1 },
+      { brand: "Ferrari", model: "Testa Rossa", color: "red", year: 2020, id: 2 },
+      { brand: "Chevrolet", model: "cavalier", color: "green", year: 2022, id: 3 },
+    ],
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hello React!</h1>
-        <Car brand="Honda" model="civic" color="blue"/>
-        <Car brand="Ferrari" model="Testa Rossa" color="red"/>
-        <Car brand="Chevrolet" model="cavalier" color="green"/>
+        <Garage cars={this.state.my_cars} />
       </div>
     );
   }
---
+++ myapp/src/components/Garage.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Garage extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        {
+          (this.props.cars.map((_car) => (
+            <div key={_car.id}>
+              <p>Brand: {_car.brand}</p>
+              <p>Model: {_car.model}</p>
+              <p>Year: {_car.year}</p>
+              <p style={{color: _car.color}}>Color: {_car.color}</p>
+              <hr />
+            </div>
+          )))
+        }
+      </div>
+    );
+  }
+}
+
+export default Garage;
